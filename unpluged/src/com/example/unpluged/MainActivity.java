1913a934e13d10778bc8c990dbefc5283e033496
Oscar Ivan Estrada <Oscar Ivan Estrada@Oscar>
2013-09-23 23:32:02 -0500

    posiciones ajustadas

--- unpluged/src/com/example/unpluged/MainActivity.java
+++ unpluged/src/com/example/unpluged/MainActivity.java
@@ -7,6 +7,7 @@ import android.media.AudioManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
@@ -28,6 +29,7 @@ public class MainActivity extends Activity implements OnSeekBarChangeListener {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
 	}
 
