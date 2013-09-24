c6bb981199a626cd18d5e558533de5b4a6bca584
Oscar Ivan Estrada <Oscar Ivan Estrada@Oscar>
2013-09-23 22:38:49 -0500

    interfaz

--- unpluged/src/com/example/unpluged/MainActivity.java
+++ unpluged/src/com/example/unpluged/MainActivity.java
@@ -28,8 +28,7 @@ public class MainActivity extends Activity implements OnSeekBarChangeListener {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//setContentView(R.layout.activity_main);
-		setContentView(R.layout.dashboard_layout);
+		setContentView(R.layout.activity_main);
 	}
 
 	@Override
